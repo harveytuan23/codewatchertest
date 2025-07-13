9cde1e8dfbbb3b6377a64c8f6b4816c5d6dc9a65
harveytuan26 <harveytuan26@gmail.com>
2025-07-12 17:16:55 -0700

    Add feature: user profile card

---
+++ cscs.py
@@ -0,0 +1,18 @@
+def foo():
+    print('hello')
+    print('world')  # 縮排錯誤
+    print('World":')
+    print('World":')
+        print('World":')
+    
+
+def bar():
+    x = 1
+    y = 2
+    # y 沒有被使用
+    
+
+def bar():
+    x = 1
+    y = 2
+    # y 沒有被使用
