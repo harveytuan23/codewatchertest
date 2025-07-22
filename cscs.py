cf15ce8e6d4bdc0e2422740d123e6dbd74856bee
harveytuan26 <harveytuan26@gmail.com>
2025-07-21 18:35:37 -0700

    test

--- cscs.py
+++ cscs.py
@@ -2,9 +2,6 @@ def foo():
     print('hello')
     print('world')  # 縮排錯誤
     print('World":')
-    print('World":')
-    print('World":')
-    print('World":')
         print('World":')
 
 def bar():
